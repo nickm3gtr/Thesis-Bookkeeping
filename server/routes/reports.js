1c3f8b483d5af2e88d595b179b4488a28b874ad7
Nick Lobaton <nickm3gtr@gmail.com>
2019-09-14 01:15:15 +0800

    updated pdf

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../models')
-const Sequelize = require('Sequelize')
 const Op = Sequelize.Op
 
 // GET journal report
